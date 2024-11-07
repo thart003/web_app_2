5d8955b02c89004c9e22f3777c1cc1a7a48069dd
thart003 <thart003@ucr.edu>
2024-11-07 12:31:01 -0800

    include DaisyUI

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,5 +14,14 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["light", "dark"], // Default themes
+    darkTheme: "dark", // Name of one of the included themes for dark mode
+    base: true, // Applies background color and foreground color for root element by default
+    styled: true, // Include daisyUI colors and design decisions for all components
+    utils: true, // Adds responsive and modifier utility classes
+    prefix: "", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
+    logs: true, // Shows info about daisyUI version and used config in console
+  },
 } satisfies Config;
